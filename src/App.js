66cd344d1f5fe2add1f48bff76458bbe2b725c24
seonye97 <sunye97@naver.com>
2023-06-24 18:00:09 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,56 +7,67 @@ function App() {
       <header className="App-header">
 
       <div className='box'>
+
+        <div>
         <ul>
           <li className='box-logo'>
-          <img className='logo' src='img/logo.png'></img>
+          <img className='logo' src='img/logo.png' alt='img'></img>
           <span className='title'>회사로고</span>
           </li>
 
           <li className='selected'>
-          <img className='icon-select' src='img/icon-01.png'></img>
-          <span className='select'>대시보드</span>
+          <img className='icon-select' src='img/icon-01.png' alt='img'></img>
+          <span className='selected-title'>대시보드</span>
           </li>
 
           <li>
-          <img className='icon' src='img/icon-02.png'></img>
+          <img className='icon' src='img/icon-02.png' alt='img'></img>
           <span className='text'>일정관리</span>
           </li>
 
           <li>
-          <img className='icon' src='img/icon-03.png'></img>
+          <img className='icon' src='img/icon-03.png' alt='img'></img>
           <span className='text'>결제관리</span>
           </li>
 
-          <li>
-          <img className='icon' src='img/icon-04.png'></img>
+          <li className='message'>
+            <div className='message-box'>
+          <img className='icon' src='img/icon-04.png' alt='img' ></img>
           <span className='text'>메시지</span>
-          <span className='number'>12</span>
-          </li>
 
+          <span class="item_num"></span>
+
+          </div>
+
+          
+
+          <div>
+          <span className='number'>12</span>
+          </div>
+          </li>
+          
+
+
+
+          </ul>
+          </div>
+
+          <div>
+           <ul>
           <li className='logout'>
-          <img className='icon' src='img/icon-05.png'></img>
+          <img className='icon' src='img/icon-05.png' alt='img'></img>
           <span className='text'>로그아웃</span>
           </li>
+          </ul>
+        
+          </div>
 
-
-        </ul>
+       
 
 
       </div>
 
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactddd
-        </a> */}
+    
       </header>
 
      
